2cd8538b4e46eb6734a82ac3c6cff6f34b41aeee
KalliLahari <85424505+KalliLahari@users.noreply.github.com>
2023-04-18 18:32:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+status1="";
+object_name="";
+
+
 function preload(){
 
 }
@@ -5,8 +9,22 @@ function preload(){
 function setup(){
 canvas=createCanvas(350,300);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(350,300);
+video.hide();
 }
 
 function draw(){
-    
+    image(video,0,0,350,300);
+}
+
+function start(){
+    objectdetector=ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+    object_name=document.getElementById("object_names").value;
+}
+
+function modelloaded(){
+    console.log("modelloaded");
+    status1="true";
 }
